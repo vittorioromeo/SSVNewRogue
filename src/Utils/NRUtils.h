296048d13c54d370a2d0a395b50a9c703b2a3b27
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 16:29:32 +0200

    license updated

--- src/Utils/NRUtils.h
+++ src/Utils/NRUtils.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2013 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
 #ifndef SSVNR_UTILS_UTILS
 #define SSVNR_UTILS_UTILS
 
