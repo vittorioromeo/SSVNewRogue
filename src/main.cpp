827866b8c5492774a52d6001f95e6ada4c65d5d9
Vittorio Romeo <vittorio.romeo@outlook.com>
2016-01-10 19:02:54 +0100

    Updated build system. Fixed new warnings by adding attributes/final keyword. TODOs.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 
-#define SSVNEWROGUE_BENCH
+// #define SSVNEWROGUE_BENCH
 
 #ifndef SSVNEWROGUE_BENCH
 
